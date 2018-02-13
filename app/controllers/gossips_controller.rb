da705de3b04b85e2372f93644338e4fe8fd1c87d
Clement <clementlecuillier@hotmail.fr>
2018-02-13 15:46:29 +0100

    gossip done

---
+++ app/controllers/gossips_controller.rb
@@ -0,0 +1,26 @@
+class GossipsController < ApplicationController
+
+  def new
+    @gossip = Gossip.new
+  end
+
+  def create
+    @gossip = Gossip.new(gossip_params)
+    @gossip.moussaillon=current_moussaillon
+    if @gossip.save
+      flash.now[:notice] = 'votre gossip a bien été créé !'
+      redirect_to gossip_path(@gossip)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    redirect_to root_path
+  end
+
+  private
+  def gossip_params
+    params.require(:gossip).permit(:content)
+  end
+end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,5 @@
 class StaticPagesController < ApplicationController
   def index
+    @gossips = Gossip.all
   end
 end
